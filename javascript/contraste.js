19dcade9a66a44f552efe7ed0f842fafa4929f4c
vaninacoria34 <vaninamariselcoria75@gmail.com>
2024-07-23 22:10:38 -0300

    REGISTRO-PERSONAL

--- javascript/contraste.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleContrastButton = document.getElementById('toggleContrast');
-    toggleContrastButton.addEventListener('click', () => {
-      document.body.classList.toggle('high-contrast');
-    });
-  });
-  
