fef3790fef2f82f399be5a90e3319ef5dd7ad48c
vaninacoria34 <vaninamariselcoria75@gmail.com>
2024-07-28 14:30:02 -0300

    Eliminar subrepositorio REGISTRO_PERSONAL

--- javascript/contraste.js
+++ javascript/contraste.js
@@ -1,8 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
+ document.addEventListener('DOMContentLoaded', () => {
     const toggleContrastButton = document.getElementById('toggleContrast');
     toggleContrastButton.addEventListener('click', () => {
       document.body.classList.toggle('high-contrast');
     });
   });
   
-        
